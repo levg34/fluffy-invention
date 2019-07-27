7eadafe09c00be2fae219a123fe5197cf5ccdcdd
levg34 <levg34@users.noreply.github.com>
2019-07-28 01:30:35 +0200

    WTF

--- js/script.js
+++ js/script.js
@@ -8,20 +8,28 @@ document.body.appendChild(renderer.domElement)
 var controls = new THREE.OrbitControls(camera, renderer.domElement)
 
 var geometry = new THREE.BoxGeometry(1, 1, 1)
-var material = new THREE.MeshBasicMaterial({color: 0xff0000})
+var material = new THREE.MeshPhongMaterial({color: 0xffffff})
 var cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
 camera.position.z = 5
 
-var light = new THREE.PointLight(0xff0000, 1, 100)
-light.position.set(50, 50, 50)
+var light = new THREE.PointLight(0x0000ff, 100, 0, 1)
+light.position.set(2, 0, 2)
 scene.add(light)
 
+var light = new THREE.PointLight(0xff0000, 100, 0, 1)
+light.position.set(-2, 0, 2)
+scene.add(light)
+
+var temps = 0
 function animate() {
-	//cube.rotation.x += 0.01
-	//cube.rotation.y += 0.01
+	cube.rotation.x += 0.01
+	cube.rotation.y += 0.01
 	requestAnimationFrame(animate)
 	renderer.render(scene, camera)
+	light.position.set(2, 1 + Math.sin(temps), 1+ Math.sin(temps));
+	temps += 0.01;
+	temps %= 10;
 }
 animate()
