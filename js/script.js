fdd95319a4cee4e86559ff9254e606e7119a54fa
levg34 <levg34@users.noreply.github.com>
2019-07-28 01:57:13 +0200

    girophare américain et cube tournant

--- js/script.js
+++ js/script.js
@@ -12,24 +12,28 @@ var material = new THREE.MeshPhongMaterial({color: 0xffffff})
 var cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 5
+camera.position.set(2,2,2)
+controls.update()
 
 var light = new THREE.PointLight(0x0000ff, 100, 0, 1)
 light.position.set(2, 0, 2)
 scene.add(light)
 
-var light = new THREE.PointLight(0xff0000, 100, 0, 1)
-light.position.set(-2, 0, 2)
-scene.add(light)
+var light2 = new THREE.PointLight(0xff0000, 100, 0, 1)
+light2.position.set(0, 2, 2)
+light2.visible = false
+scene.add(light2)
 
-var temps = 0
+var n = 0
 function animate() {
 	cube.rotation.x += 0.01
 	cube.rotation.y += 0.01
 	requestAnimationFrame(animate)
 	renderer.render(scene, camera)
-	light.position.set(2, 1 + Math.sin(temps), 1+ Math.sin(temps));
-	temps += 0.01;
-	temps %= 10;
+	if (n++ > 15) {
+		n=0
+		light.visible = !light.visible
+		light2.visible = !light2.visible
+	}
 }
 animate()
